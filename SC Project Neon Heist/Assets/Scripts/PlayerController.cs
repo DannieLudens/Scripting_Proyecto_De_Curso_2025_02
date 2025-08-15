db09a575832371e8712ab1d22530f5cfa0325586
SofiaLezcanoArenas <sofia.lezcano.arenas@gmail.com>
2025-08-14 20:59:30 -0500

    Movimiento horizontal Jayce implementado
    El movimiento de Jayce funciona con las teclas derecha izquierda, pero aún es bastante rápido y descontrolado

--- SC Project Neon Heist/Assets/Scripts/PlayerController.cs
+++ SC Project Neon Heist/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+            horizontal = 1.0f;
+        }
+
+        Debug.Log(horizontal);//para observar la variable en la consola de unity
+        Vector2 posicion = transform.position;
+        posicion.x = posicion.x + 0.1f * horizontal;
+        transform.position = posicion;
     }
 }
